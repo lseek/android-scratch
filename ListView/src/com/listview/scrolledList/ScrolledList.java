7e6e1fdc7465ae16c2b430948c2862ae5760905b
lseek <jayworkapp@gmail.com>
2014-04-11 08:36:06 +0800

    Use custom ArrayAdapter
    Use a custom ArrayAdapter to control which elements of the ListView are
    disabled. Still have problems with the display - even though getView is
    called for each list item on the screen, in some cases the text color is
    set to the color of a disabled item.  It's as if the view were recycled.

--- ListView/src/com/listview/scrolledList/ScrolledList.java
+++ ListView/src/com/listview/scrolledList/ScrolledList.java
@@ -1,20 +1,59 @@
 package com.listview.scrolledList;
 
 import android.content.res.Resources;
+import android.content.Context;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
 import android.graphics.Color;
+import android.util.SparseBooleanArray;
+import android.util.Log;
+import java.util.List;
 
+
+class CustomAdapter extends ArrayAdapter<String> {
+    private SparseBooleanArray enabledItems = new SparseBooleanArray();
+
+    public CustomAdapter(Context context, int textViewResourceId, String[] objects) {
+        super(context, textViewResourceId, objects);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Log.d("ListViewScrolledList", String.format("getView(%d)", position));
+        TextView item = (TextView) super.getView(position, convertView, parent);
+        if (!isEnabled(position)) {
+            Log.d("ListViewScrolledList", String.format("Position:%d is not enabled", position));
+            item.setTextColor(Color.parseColor("#00ff00"));
+        }
+        return item;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return enabledItems.get(position, true);
+    }
+
+    public void disableItem(int position) {
+        boolean state = enabledItems.get(position, true);
+        enabledItems.put(position, false);
+        Log.d("ListViewScrolledList", String.format("Disabled:%d", position));
+    }
+}
 
 public class ScrolledList extends Activity {
     Resources res;
     TextView infoArea;
     ListView items;
-    ArrayAdapter<CharSequence> entries;
-    TextView currItem;
+    CustomAdapter entries;
+    String[] inStrs;
+    int currIdx;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,25 +61,31 @@ public class ScrolledList extends Activity {
         setContentView(R.layout.main);
 
         res = getResources();
+        inStrs = res.getStringArray(R.array.lorem);
+
         items = (ListView)findViewById(R.id.wordList);
-        entries = ArrayAdapter.createFromResource(this, R.array.lorem, android.R.layout.simple_list_item_1);
+        entries = new CustomAdapter(this, android.R.layout.simple_list_item_1, inStrs);
         items.setAdapter(entries);
         items.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         items.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                currItem = (TextView)view;
+                TextView currItem = (TextView)view;
                 infoArea.setText(String.format("Selection:%s", currItem.getText().toString()));
-                currItem.setEnabled(false);
+                currIdx = position;
             }
         });
 
         infoArea = (TextView)findViewById(R.id.infoArea);
-        infoArea.setBackgroundColor(Color.parseColor("#ffffaa"));
+        infoArea.setBackgroundColor(Color.parseColor("#666600"));
 
     }
 
     public void disableEntry(View disableBtn) {
-        currItem.setEnabled(false);
+        Log.d("ListViewScrolledList", String.format("Disabling:%d", currIdx));
+        entries.disableItem(currIdx);
+        Log.d("ListViewScrolledList", String.format("notifyDataSetChanged:%d", currIdx));
+        entries.notifyDataSetChanged();
+        Log.d("ListViewScrolledList", String.format("disableEntry:%d done", currIdx));
     }
 }
